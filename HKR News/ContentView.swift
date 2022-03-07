d383d8baff74e2f4456d2001b919420c21060f70
Dario Mintzer <dmintzer75@hotmail.com>
2022-03-07 14:36:07 -0300

    Initial Commit

---
+++ HKR News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HKR News
+//
+//  Created by Dario Mintzer on 07/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HKR News/HKR_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HKR_NewsApp.swift
+//  HKR News
+//
+//  Created by Dario Mintzer on 07/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct HKR_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
